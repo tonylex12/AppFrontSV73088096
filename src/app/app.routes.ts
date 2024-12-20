e8fca7dbc6f7b7a89cb4558b59e5ad7a32818146
Tony HUerto <tonylex12@gmail.com>
2024-12-20 13:23:22 -0500

    primera pregunta terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { MaterialModule } from './angular-material/material/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
+import { HomeComponent } from './dashboard/home/home.component';
+import { UsersComponent } from './dashboard/users/users.component';
+import { PostsComponent } from './dashboard/posts/posts.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'posts', component: PostsComponent },
+    ],
+  },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
